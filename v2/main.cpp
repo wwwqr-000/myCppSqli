83104895a098b6c954a00feba83fe215fe1acc50
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2024-04-05 19:57:19 +0200

    v2 update

--- v2/main.cpp
+++ v2/main.cpp
@@ -3,7 +3,7 @@
 #include "classes/Mysql.hpp"
 
 int main() {
-    Mysql conn = Mysql("https://ipinfo.io", {"Content-Type: application/json"});
+    Mysql conn = Mysql("https://ipinfo.io", {"Content-Type: application/json"}, "test_db");
 
     std::cout << conn.getIndex() << "\n";
     return 0;
